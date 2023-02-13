05b334541e7b901d67a1ffb01b43c3b31fd2e1e2
godbehere <godbehere@gmail.com>
2023-02-13 14:50:46 -0500

    Added 'Shopper UI Project and image Updated descriptions for several projects

--- src/projects-obj.js
+++ src/projects-obj.js
@@ -5,9 +5,10 @@ export const projectObj = [
         tech: [
             <li>JavaScript</li>,
             <li>React</li>,
-            <li>Python (Flask)</li>
+            <li>Python (Flask)</li>,
+            <li>Docker</li>
             ],
-        description: 'Web app that uses threejs to animate furniture assembly in order to assist customers',
+        description: 'Furniture Assembly is an application which makes use of the threejs JavaScript library to animate Ikea furniture assembly. The models were downloaded from Ikea’s GitHub page and are served via a Flask API running in a Docker container with Gunicorn.',
         link: 'https://ikea.godbehere.org',
         github: 'https://github.com/gregyoung03/I_WebApp',
     },
@@ -37,15 +38,33 @@ export const projectObj = [
         github: 'https://github.com/godbehere/adventures-in-food',
     },
     {
+        title: 'Shopper UI',
+        pic: process.env.PUBLIC_URL + './assets/images/store-g40f30356f_1280.png',
+        tech: [
+            <li>HTML</li>,
+            <li>CSS</li>,
+            <li>Java</li>,
+            <li>TypeScript</li>,
+            <li>Angular</li>,
+            <li>mySQL</li>,
+            <li>SpringBoot</li>,
+            <li>Docker</li>
+            ],
+        description: 'Shopper UI page created as part of training at FDM. Uses RESTful API written with SpringBoot framework.',
+        link: 'http://shopper.godbehere.org/',
+        github: '',
+    },
+    {
         title: 'Largo Farms Website',
         pic: process.env.PUBLIC_URL + './assets/images/barley-field-ge75bde247_1280.jpg',
         tech: [
             <li>Angular</li>,
             <li>TypeScript</li>,
             <li>Python (Flask)</li>,
-            <li>MongoDB</li>
+            <li>MongoDB</li>,
+            <li>Docker</li>
             ],
-        description: 'Shop page for a local hobby farm that sells chicken and beef products.',
+        description: 'Largo Farms Website is an application which I created while learning Flask, Angular, and MongoDB. This site will eventually replace my family farm’s current website, but in the meantime has been a wonderful learning opportunity. I built the frontend using Angular (TypeScript) and keep the cart and product list up to date with observables. The cart and inventory data are stored in MongoDB which is running in a Docker container. The API for the database was implemented with Flask (Python)',
         link: 'https://largo.godbehere.org',
         github: 'https://github.com/godbehere/largo-farm',
     },
